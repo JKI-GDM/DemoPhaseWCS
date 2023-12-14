ab2de4a28378e4669be2bbddda4b3067facf3769
JKI's Geodata Management & Analysis <127733040+JKI-GDM@users.noreply.github.com>
2023-12-14 14:12:44 +0100

    added credentials.py
    added credentials.py to faciliate usage of credentials for data cubes

---
+++ credentials.py
@@ -0,0 +1,21 @@
+'''
+First you have to generate file called credentials.py
+file content:
+
+ras_user = 'your user name'
+ras_pw = 'your password'
+ras_host = 'host ip'
+
+ras_cde_user = 'your user name'
+ras_cde_pw = 'your password'
+ras_cde_host = 'host ip'
+'''
+# rasdman
+ras_user=''
+ras_pw=''
+ras_host = 'https://datacube.julius-kuehn.de/flf/ows'
+# code-de
+# rasdman CODE-DE
+ras_cde_user=''
+ras_cde_pw=''
+ras_cde_host='https://code-de.rasdaman.com/rasdaman/ows'
